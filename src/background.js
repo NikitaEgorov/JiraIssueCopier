eb3be9377ef52b704fd5f9dad8b7cdd56127aba7
egorovnikita <egorovnikita@ozon.ru>
2024-07-02 09:50:19 +0300

    Fix for new jira

--- src/background.js
+++ src/background.js
@@ -1,20 +1,19 @@
 async function getCurrentTab() {
-    const queryOptions = { active: true, lastFocusedWindow: true };
-    const [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
+  const queryOptions = { active: true, lastFocusedWindow: true };
+  const [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+const copyToClipboardMsg = async () => {
+  const currentTab = await getCurrentTab();
+  if (currentTab)
+    chrome.tabs.sendMessage(currentTab.id, {
+      message: "jira_copy_to_clipboard",
+    });
+};
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "copy-to-clipboard") {
+    copyToClipboardMsg();
   }
-  
-  const copyToClipboardMsg = async () => {
-    const currentTab = await getCurrentTab();
-    if (currentTab)
-      chrome.tabs.sendMessage(currentTab.id, {
-        message: "jira_copy_to_clipboard",
-      });
-  };
-  
-  chrome.commands.onCommand.addListener((command) => {
-    if (command === "copy-to-clipboard") {
-      copyToClipboardMsg();
-    }
-  });
-  
+});
--- src/jira-toast.js
+++ src/jira-toast.js
@@ -1,14 +1,13 @@
 function showToast({ title, body }) {
-    window.AJS.flag({
-      type: "success",
-      close: "auto",
-      title,
-      body,
-    });
-  }
-  
-  window.addEventListener("message", (evt) => {
-    if (evt.data.type === "jira_success_copied_to_clipboard")
-    showToast(evt.data.options);
+  window.AJS.flag({
+    type: "success",
+    close: "auto",
+    title,
+    body,
   });
-  
+}
+
+window.addEventListener("message", (evt) => {
+  if (evt.data.type === "jira_success_copied_to_clipboard")
+  showToast(evt.data.options);
+});
